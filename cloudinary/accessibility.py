a8b64fa341f62ef855651831fd2e41160bcba68b
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2019-12-12 14:30:40 -0800

    Updated the function to fetch the cloudinary credentials from within itself and renamed worker_url to remote_url to make it more generic

--- cloudinary/accessibility.py
+++ cloudinary/accessibility.py
@@ -2,6 +2,8 @@ import logging
 from cloudinary.http_client import HttpClient
 from cloudinary.utils import json_encode
 
+import cloudinary
+
 logger = logging.getLogger("Cloudinary")
 ch = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -10,11 +12,8 @@ logger.addHandler(ch)
 
 
 def fetch_alt_tags(
-        cloud_name,
-        api_key,
-        api_secret,
         public_id,
-        worker_url
+        remote_url
 ):
     """
     A wrapper function that makes a call to the remote URL specified as worker_url and fetches the alt text tag.
@@ -26,23 +25,19 @@ def fetch_alt_tags(
 
     It may have any other dictionary along with this value but, alt_text is the only mandatory value.
 
-    :param cloud_name: Cloudinary Cloud name for the resource
-    :param api_key: API Key to use for fetching the context metadata
-    :param api_secret: API Key to use for fetching the context metadata
-    :param public_id: The actual resource for which we need the alt_text information
-    :param worker_url: The remote service that will make the Cloudinary API call and return the alt_text
+    :param public_id: Object for which we require the alt-text
+    :param remote_url: API endpoint that will be returning the alt-text information
 
-    :return: A dictionary object that will return the alt-text value if it was defined for the resource
+    :return: JSON object that will include the alt-text if one is present
     """
-
-    logger.debug(u"Cloud Name: {}, Worker URL: {}, Public ID: ".format(cloud_name, worker_url,public_id))
-
     body = {
-        "cloudName": cloud_name,
-        "publicId": public_id,
-        "apiKey": api_key,
-        "apiSecret": api_secret
+        "cloudName": cloudinary.config().cloud_name,
+        "apiKey": cloudinary.config().api_key,
+        "apiSecret": cloudinary.config().api_secret,
+        "publicId": public_id
     }
     client = HttpClient()
-    response = client.post_json(worker_url, json_encode(body))
+    response = client.post_json(remote_url, json_encode(body))
     return response
+
+
